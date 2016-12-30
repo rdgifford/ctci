c6dc700e6ca4c3272f7a71e70ae364d6a600d497
rdgifford <robbie.gifford@gmail.com>
2016-12-29 23:00:55 -0800

    Add partial fix for in place rotateMatrix, but still stuck on ordering

--- arrays_strings/rotateMatrix.js
+++ arrays_strings/rotateMatrix.js
@@ -6,15 +6,17 @@ var matrix = [
   [7, 8, 9]
 ];
 
-// 0 0 , 0 1 , 0 2
-// 1 0 , 1 1 , 1 2
-// 2 0 , 2 1 , 2 2
+// 0 0 , 0 1 , 0 2 , 0 3
+// 1 0 , 1 1 , 1 2 , 1 3
+// 2 0 , 2 1 , 2 2 , 2 3
+// 3 0 , 3 1 , 3 2 , 3 3
 //
 // >>>
 //
-// 2 0 , 1 0 , 0 0
-// 2 1 , 1 1 , 0 1
-// 2 2 , 1 2 , 0 2
+// 3 0 , 2 0 , 1 0 , 0 0
+// 3 1 , 2 1 , 1 1 , 0 1
+// 3 2 , 2 2 , 1 2 , 0 2
+// 3 3 , 2 3 , 1 3 , 0 3
 
 var rotatedMatrix = [
   [7, 4, 1],
@@ -24,18 +26,31 @@ var rotatedMatrix = [
 
 // how to you do this in place?
 // swap with var like typical swap function
+// pattern is unique -- run to last element of column or row
+
+// naive solution
+// var rotateMatrix = (matrix) => {
+//   var m = [];
+//   for (var i = 0, l = matrix.length; i < l; i++) {
+//     m[i] = [];
+//     var counter = 0;
+//     for (var x = matrix[i].length - 1; x >= 0; x--) {
+//       m[i][counter] = matrix[x][i];
+//       counter++;
+//     }
+//   }
+//   return m;
+// }
 
 var rotateMatrix = (matrix) => {
-  var m = [];
-  for (var i = 0, l = matrix.length; i < l; i++) {
-    m[i] = [];
-    var counter = 0;
-    for (var x = matrix[i].length - 1; x >= 0; x--) {
-      m[i][counter] = matrix[x][i];
-      counter++;
+  // how do you replace specific parts of the matrix?
+  // spin around matrix, swap for each space
+  for(var row = 0; row < matrix.length; row++) {
+    // run col or row, splice all out & in
+    for(var col = 0; col < row.length; col++) {
+      matrix[row][col];
     }
   }
-  return m;
 }
 
 it('rotateMatrix', () => {
